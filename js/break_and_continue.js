44c430f8ce70e26c2d7dd827831e722d406b7e6e
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-07-31 09:40:16 -0500

    Commit of edit in if(userInput) by containing it between 1 and 50

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -18,7 +18,7 @@
  // }
 while(true){
     var userInput = parseFloat(prompt("Please enter an odd number between 1 and 50"));
-    if(userInput % 2 !== 0){
+    if(userInput % 2 !== 0 && userInput > 0 && userInput <= 50){
         break;
     }
 }
