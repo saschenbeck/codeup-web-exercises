0fe7eca26131e54401f7be7fc3520f331e6abc24
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-03 09:16:57 -0500

    Completion of planets-array to-dos

--- js/planets-array.js
+++ js/planets-array.js
@@ -19,22 +19,43 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
-    console.log(planets);
+    console.log("Before unshift", planets);
+    planets.unshift("The Sun");
+    console.log("After unshift", planets);
+
+    console.log("_______________________________________");
 
     console.log('Adding "Pluto" to the end of the planets array.');
-    console.log(planets);
+    console.log("Before push", planets);
+    planets.push("Pluto");
+    console.log("After push", planets);
+
+    console.log("_______________________________________");
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
-    console.log(planets);
+    console.log("Before shift", planets);
+    planets.shift();
+    console.log("After shift", planets);
+
+    console.log("_______________________________________");
 
     console.log('Removing "Pluto" from the end of the planets array.');
-    console.log(planets);
+    console.log("Before pop", planets);
+    planets.pop();
+    console.log("After pop", planets);
+
+    console.log("_______________________________________");
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log("The index of Earth is " + planets.indexOf("Earth"));
+
+    console.log("_______________________________________");
 
     console.log("Reversing the order of the planets array.");
-    console.log(planets);
+    console.log(planets.reverse());
+
+    console.log("_______________________________________");
 
     console.log("Sorting the planets array.");
-    console.log(planets);
+    console.log(planets.sort());
 })();
