efbb711d24ca238bc1f58aa8723c5170f26ff9a7
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-09-14 10:01:58 -0500

    feat: json format practice

---
+++ lecture-practice/json-formatting-practice.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var cohort;
+cohort = {
+    'name': 'Bash',
+    'number_of_students': 2,
+    'location': 'Dallas, TX',
+    'students': [
+        {
+            'name': 'Stacy R',
+            'dob': '1990-01-24',
+            'scholarship': true,
+            'capstone': null
+        },
+        {
+            'name': 'Juan M',
+            'dob': '1992-05-13',
+            'scholarship': false,
+            'capstone': null
+        }
+    ],
+    'remote': false,
+    'instructors': ['Fernanado M', 'Douglas H']
+};
