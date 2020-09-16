6bdd66887d8c98d552ade3ecbda1d3bed3ca4a6d
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-09-16 08:44:14 -0500

    feat: working on styling weather forecast

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,42 +1,138 @@
 "use strict";
-
 (function () {
-    $(document).ready(function () {
-        var opwUrl = "http://api.openweathermap.org/data/2.5/forecast"
-        $.get(opwUrl, {
-            "APPID": OPW_API,
-            "q": "Irving, TX, US",
-            "units": "imperial",
-        }).done(function (forecast) {
-            console.log("5 Day Forecast", forecast);
+
+    mapboxgl.accessToken = mapboxToken;
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [-96.94, 32.82], // starting position [lng, lat]
+        zoom: 12,  // starting zoom
+    });
+
+    function getWeatherData(lng, lat) {
+
+        $.get("http://api.openweathermap.org/data/2.5/forecast", {
+            APPID: OPW_API,
+            lon: lng,
+            lat: lat,
+            units: "imperial"
+        }).done(function (data) {
+            console.log(data);
+            var fiveDayForecast = [];
+            for (var i = 0; i < data.list.length; i += 8) {
+                fiveDayForecast.push(data.list[i]);
+            }
+
+            $("#city").text("Current city: " + data.city.name);
+
+            $("#forecast").html("");
+
+            for (let day of fiveDayForecast) {
+                createDayCard(day);
+            }
+
+            var days = $("#forecast>.day");
+
+            var fadeAllIn = function test(index) {
+                $("#template").css("display", "none");
+                $(days[index]).slideDown(400, function() {
+                    if (index + 1 < days.length) {
+                        fadeAllIn(index + 1);
+                    }
+                });
+            }
+
+            fadeAllIn(0);
+
         });
+    }
+
+    function createDayCard(day) {
+        var date = day.dt_txt;
+        var maxTemp = day.main.temp_max.toString();
+        var minTemp = day.main.temp_min.toString();
+        var icon = day.weather[0].icon;
+        var description = day.weather[0].description;
+        var humidity = day.main.humidity;
+        var wind = day.wind.speed;
+        var pressure = day.main.pressure;
+        var card = document.createElement("div");
+
+        $(card).addClass("day card col-sm-2")
+            .html($("#template").html());
+
+        $(card).find(".temperature").text(minTemp + " °F / " + maxTemp + " °F");
+        $(card).find(".date").text(date);
+        $(card).find(".icon").html(
+            "<img src='http://openweathermap.org/img/w/" + icon + ".png'>"
+        )
+
+        $(card).find(".description").html(description);
+
+        $(card).find(".humidity").html(
+            "<strong>Humidity: </strong>" + humidity + "%"
+        )
+
+        $(card).find(".wind").html(
+            "<strong>Wind: </strong>" + wind + " mph"
+        )
+
+        $(card).find(".pressure").html(
+            "<strong>Pressure: </strong>" + pressure
+        )
+
+        $("#forecast").append($(card).hide());
+    }
+
+    $("#citySubmit").click(function (e) {
+        e.preventDefault();
+        var location = $("#cityInput").val().trim();
+        if (location !== "") {
+            searchFor(location);
+        }
     });
-})();
-mapboxgl.accessToken = mapboxToken;
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-96.9489, 32.814], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
 
-function placeMarkerAndPopup(info, token, map) {
-    geocode(info, token).then(function(coordinates) {
-        var popup = new mapboxgl.Popup().setHTML(info.popupHTML);
-        var marker = new mapboxgl.Marker()
-            .setLngLat(coordinates)
-            .addTo(map)
-            .setPopup(popup);
-        popup.addTo(map);
+    //Attempted to use .submit to stop the return key from refreshing the page
+    // $("#cityInput").submit(function (e) {
+    //     e.preventDefault()
+    // })
+
+    $("#cityInput").on("keypress", function(e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            var location = $("#cityInput").val().trim();
+            if (location !== "") {
+                searchFor(location);
+            }
+        }
     });
-};
 
+    function searchFor(location) {
 
+        geocode(location, mapboxToken)
+            .then(function (result) {
+                getWeatherData(result[0], result[1]);
+                return result;
+            }).then(function (data) {
+            map.flyTo({center: data, zoom: 12});
 
-placeMarkerAndPopup("Irving, TX", mapboxgl.accessToken, map);
+            var marker = new mapboxgl.Marker({
+                draggable: true})
+                .setLngLat(data)
+                .addTo(map);
 
-$("#weatherLocationSubmit").click(function (e) {
-    e.preventDefault();
-    console.log("success");
-})
+            function onDragEnd() {
+                var lngLat = marker.getLngLat();
+
+                getWeatherData(lngLat.lng, lngLat.lat);
+                map.flyTo({center: [lngLat.lng, lngLat.lat], zoom: 12});
+            }
+
+            marker.on('dragend', onDragEnd);
+        });
+    }
+
+    searchFor("Irving, TX");
+
+}());
 
