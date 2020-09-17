04b67ae2ce468e7be9f2284c012525be8ed43988
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-09-17 16:27:00 -0500

    adjusted break and continue js issue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,33 +1,33 @@
 "use strict";
 
- // while(true) {
- //     var userInput = parseFloat(prompt("Please enter an odd number between 1 and 50."));
- //     for (var i = 0; i <= 50; i++) {
- //         if (i % 2 !== 0) {
- //             if (i === userInput) {
- //                 continue;
- //             }
- //         }else{
- //             userInput;
- //         }
- //         console.log(i);
- //     }
- //     if (userInput % 2 !== 0) {
- //         break;
- //     }
- // }
+// while(true) {
+//     var userInput = parseFloat(prompt("Please enter an odd number between 1 and 50."));
+//     for (var i = 0; i <= 50; i++) {
+//         if (i % 2 !== 0) {
+//             if (i === userInput) {
+//                 continue;
+//             }
+//         }else{
+//             userInput;
+//         }
+//         console.log(i);
+//     }
+//     if (userInput % 2 !== 0) {
+//         break;
+//     }
+// }
 while(true){
-    var userInput = parseFloat(prompt("Please enter an odd number between 1 and 50"));
-    if(userInput % 2 !== 0 && userInput > 0 && userInput <= 50){
-        break;
-    }
+  var userInput = parseFloat(prompt("Please enter an odd number between 1 and 50"));
+  if(userInput % 2 !== 0 && userInput > 0 && userInput <= 50){
+    break;
+  }
 }
 console.log("Number to skip is " + userInput)
 for(var i = 0; i <=50; i++){
-    if(i % 2 !==0){
-        if(i === userInput){
-            continue
-        }
-        console.log("Here is an odd number: " + i);
+  if(i % 2 !==0){
+    if(i === userInput){
+      continue
     }
+    console.log("Here is an odd number: " + i);
+  }
 }
